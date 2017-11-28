35daa383a2d7e6f0d6535b1431140da3e1c8595d
RostislavKorostelev <korrost@mail.ru>
2017-11-28 21:29:32 +0400

    added 10 task

--- src/buildings/Buildings.java
+++ src/buildings/Buildings.java
@@ -7,6 +7,8 @@ import buildings.interfaces.Floor;
 import buildings.interfaces.Space;
 
 import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Comparator;
 import java.util.Formatter;
 import java.util.Scanner;
@@ -202,14 +204,182 @@ public class Buildings {
         return buildingFactory.createBuilding(floors);
     }
 
-    public static Floor createFloor(int spaceCount){ return buildingFactory.createFloor(spaceCount);}
+    public static Building createBuilding(int floorsCount, int[] spacesCounts){
+        return buildingFactory.createBuilding(floorsCount, spacesCounts);
+    }
+
+    public static Floor createFloor(int spaceCount){
+        return buildingFactory.createFloor(spaceCount);
+    }
     public static Floor createFloor(Space...spaces){
         return buildingFactory.createFloor(spaces);
     }
 
+    public static Space createSpace (int rooms, float area){
+        return buildingFactory.createSpace(rooms, area);
+    }
+
+    public static Space createSpace (float area){
+        return buildingFactory.createSpace(area);
+    }
+
     public static Floor synchronizedFloor (Floor floor){
         return new SynchronizedFloor(floor);
-
     }
+
+
+    //Ref
+
+    public static Space createSpace (float area, Class spaceClass){
+        try{
+            Class space = Class.forName(spaceClass.getName());
+            Constructor constructor = space.getConstructor(new Class[]{float.class});
+            Space res = (Space) constructor.newInstance(area);
+            return res;
+        }
+        catch (ClassNotFoundException | NoSuchMethodException | IllegalArgumentException | InvocationTargetException | IllegalAccessException | InstantiationException e){
+            e.getMessage();
+        }
+        return null;
+    }
+
+    public static Space createSpace (int rooms, float area, Class spaceClass){
+        try{
+           Class space = Class.forName(spaceClass.getName());
+           Constructor constructor = space.getConstructor(new Class[]{float.class, int.class});
+           Space res = (Space) constructor.newInstance(area, rooms);
+           return res;
+        }
+        catch (ClassNotFoundException | NoSuchMethodException | IllegalArgumentException | InvocationTargetException | IllegalAccessException | InstantiationException e){
+            e.getMessage();
+        }
+
+        return null;
+    }
+
+    public static Floor createFloor(int spaceCount, Class floorClass){
+        try{
+            Class floor = Class.forName(floorClass.getName());
+            Constructor constructor = floor.getConstructor(new Class[]{int.class});
+            Floor res = (Floor) constructor.newInstance(spaceCount);
+            return res;
+        }
+        catch (ClassNotFoundException | NoSuchMethodException | IllegalArgumentException | InvocationTargetException | IllegalAccessException | InstantiationException e){
+            e.getMessage();
+        }
+        return null;
+    }
+
+    public static Floor createFloor(Space[] spaces, Class floorClass){
+        try{
+            Class floor = Class.forName(floorClass.getName());
+            Constructor constructor = floor.getConstructor(new Class[]{Space[].class});
+            Floor res = (Floor) constructor.newInstance((Object) spaces);
+            return res;
+        }
+        catch (ClassNotFoundException | NoSuchMethodException | IllegalArgumentException | InvocationTargetException | IllegalAccessException | InstantiationException e){
+            e.getMessage();
+        }
+        return null;
+    }
+
+    public static Building createBuilding(Floor[] floors, Class bClass){
+        try{
+            Class building = Class.forName(bClass.getName());
+            Constructor constructor = building.getConstructor(new Class[]{Floor[].class});
+            Building res = (Building) constructor.newInstance((Object) floors);
+            return res;
+        }
+        catch (ClassNotFoundException | NoSuchMethodException | IllegalArgumentException | InvocationTargetException | IllegalAccessException | InstantiationException e){
+            e.getMessage();
+        }
+        return null;
+    }
+
+    public static Building createBuilding(int floorsCount, int[] spacesCounts, Class bClass){
+        try{
+            Class building = Class.forName(bClass.getName());
+            Constructor constructor = building.getConstructor(new Class[]{int.class,int[].class});
+            Building res = (Building) constructor.newInstance(floorsCount, spacesCounts);
+            return res;
+        }
+        catch (ClassNotFoundException | NoSuchMethodException | IllegalArgumentException | InvocationTargetException | IllegalAccessException | InstantiationException e){
+            e.getMessage();
+        }
+        return null;
+    }
+
+
+    public static Building inputBuilding (InputStream in, Class buildingClass, Class floorClass, Class spaceClass) throws IOException
+    {
+        Building newBuilding = null;
+        DataInputStream streamRead = new DataInputStream(in);
+        try
+        {
+            int floorsCount = streamRead.readInt();
+            Floor[] floors = new Floor[floorsCount];
+            for (int i = 0; i < floors.length; i++) {
+                floors[i] = createFloor(streamRead.readInt(), floorClass); //////
+                for (int j = 0; j <floors[i].getCnt() ; j++) {
+                    Space space = createSpace(streamRead.readInt(),streamRead.readFloat(), spaceClass);
+
+                    floors[i].setSpace(j, space);
+                }
+            }
+            newBuilding = createBuilding(floors, buildingClass);
+        }
+        catch (IOException ioex)
+        {
+            ioex.getMessage();
+        }
+        return newBuilding;
+    }
+
+    public static Building readBuilding (Reader in, Class bClass, Class fClass, Class sClass) throws IOException {
+        Building newBuilding = null;
+        try {
+            StreamTokenizer streamTokenizer = new StreamTokenizer(in);
+            while (streamTokenizer.nextToken() != streamTokenizer.TT_NUMBER) {
+            }
+            Floor[] floors = new Floor[(int) streamTokenizer.nval];
+            for (int i = 0; i < floors.length; i++) {
+                while (streamTokenizer.nextToken() != streamTokenizer.TT_NUMBER) {
+                }
+                floors[i] = createFloor((int) streamTokenizer.nval, fClass);
+                for (int j = 0; j < floors[i].getCnt(); j++) {
+                    while (streamTokenizer.nextToken() != streamTokenizer.TT_NUMBER) {
+                    }
+                    int sI = (int) streamTokenizer.nval;
+                    while (streamTokenizer.nextToken() != streamTokenizer.TT_NUMBER) {
+                    }
+                    float sF = (float) streamTokenizer.nval;
+                    floors[i].setSpace(j,createSpace(sI,sF,sClass));
+                }
+            }
+            newBuilding = createBuilding(floors, bClass);
+        } catch (IOException ioex) {
+            ioex.getMessage();
+        }
+        return newBuilding;
+    }
+
+    public static Building readBuilding (Scanner scanner, Class bClass, Class fClass, Class sClass) throws IOException
+    {
+
+        Floor[] floors = new Floor[scanner.nextInt()];
+        for (int i = 0; i <floors.length ; i++) {
+            floors[i] =createFloor(scanner.nextInt(), fClass);
+            for (int j = 0; j <floors[i].getCnt() ; j++) {
+                floors[i].setSpace(j, createSpace(scanner.nextInt(), scanner.nextFloat(), sClass));
+            }
+        }
+        return createBuilding(floors, bClass);
+    }
+
+
+
+
+
+
 
 }
